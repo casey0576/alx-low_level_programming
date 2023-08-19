6fd670870a6b04f43d361cdff65fec7ccaec830f
casey0576 <caseyisaac576@gmail.com>
2023-08-19 09:22:11 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,12 @@
 
 /**
  * print_name - prints a given name
- * @name: The name  to be printed
- * @f: function name
- * Return: void
+ * @name: prints a person's name
+ * @f: prints a function
+ * Return: always void
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	f(name);
+		f(name);
 }
