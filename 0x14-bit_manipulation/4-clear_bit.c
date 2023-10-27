cf7ba4d8335cadfbc78328130920eb0febdbdee6
casey0576 <caseyisaac576@gmail.com>
2023-10-27 13:00:49 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,13 +9,13 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
-	
 	mask = 1UL << index;
-	
+
 	if (*n & mask)
 	{
 		*n ^= mask;
