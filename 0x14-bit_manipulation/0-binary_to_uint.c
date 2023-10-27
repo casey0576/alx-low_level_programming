11589c361c85d00f9e7ef9495f4d33de2d0d8ccb
casey0576 <caseyisaac576@gmail.com>
2023-10-27 10:51:47 -0700

    0binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,23 @@
 
 unsigned int binary_to_unit(const char *b)
 {
-    int c;
-    unsigned int number;
+	int c;
+	unsigned int number;
 
-    number = 0;
-    if (!b)
-    return (0);
-    for (c = 0; b[c] != '\0'; c++)
-    {
-        if (b[c] != '0' && b[c] != '1')
-        return (0);
-    }
-    for (c = 0; b[c] != '\0'; c++)
-    {
-        number <<= 1;
-        if (b[c] == '1')
-        number += 1;
-    }
-    return(number);
+	number = 0;
+	if (!b)
+		return (0);
+
+	for (c = 0; b[c] != '\0'; c++)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+	}
+	for (c = 0; b[c] != '\0'; c++)
+	{
+		number <<= 1;
+		if (b[c] == '1')
+			number += 1;
+	}
+	return (number);
 }
